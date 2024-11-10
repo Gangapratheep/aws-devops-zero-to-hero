dabf0101ddb7c44b7f8621a6ce733ee3d7365abe
Gangapratheep <129517386+Gangapratheep@users.noreply.github.com>
2024-11-10 11:22:02 +0400

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,7 +5,7 @@ set -e
 docker pull gangapratheep/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 gangapratheep/simple-python-flask-app 
+docker run -d -p 5000:5000 gangapratheep/simple-python-flask-app   
 
 
 
